1cf5d5a3ed53521d1b48b913ee11b21862d4a329
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2018-05-18 00:00:11 +0100

    adds css for the list

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import './style/style.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import ApolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 import { HashRouter } from 'react-router-dom'
