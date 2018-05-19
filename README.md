894efbad3c5e967f9e46a1c06d690370f36e6182
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2018-05-19 18:38:21 +0100

    lyric likes and optimistic ui

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lyrical-GraphQL
 Starter project from a GraphQL course on Udemy.com
+
+This is a basic example of how graphql is used on the client side.
--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
 class LyricList extends Component {
 
+  onLike(id, likes) {
+    //in the below mutation, optimisticResponse will provide guessed response before data in ui is actually updated, so that the user thinks the data is instantly updated.
+    this.props.mutate({ 
+      variables: { id },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          id: id,
+          __typename: 'LyricType',
+          likes: likes + 1
+        }
+      }
+    })
+      // .then(()=> this.props.data.refetch() )
+  }
+
   renderLyrics() {
     const { lyrics } = this.props
-    return lyrics.map(({id, content})=>{
+    // console.log("lyrics>>", lyrics)
+    // console.log("LyricList::this.props>>", this.props)
+    // Make sure to have added "likes" in queries "fetchSong" for the likes to show up.
+    return lyrics.map(({id, content, likes})=>{
       return (
         <li key={id} className="collection-item">
           {content}
+          <div className="vote-box">
+            <i
+              className="material-icons"
+              onClick={()=>this.onLike(id, likes)}
+            >
+              thumb_up
+            </i>
+            {likes}
+          </div>
         </li>
       )
     })
@@ -23,4 +53,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`
+
+export default graphql(mutation)(LyricList)
